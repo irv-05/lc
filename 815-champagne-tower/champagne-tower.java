631b3a6e898c31e987aac30ee08a9c30ac2a869c
irv-05 <irv05code@gmail.com>
2026-02-20 16:54:42 -0800

    Time: 4 ms (28.00%) | Memory: 48.4 MB (41.84%) - LeetSync

--- 815-champagne-tower/champagne-tower.java
+++ 815-champagne-tower/champagne-tower.java
@@ -1,20 +1,19 @@
 class Solution {
     public double champagneTower(int poured, int query_row, int query_glass) {
-        List<Double> cur = new ArrayList<>();
-        cur.add(0.0 + poured);
+        Double[] cur = new Double[query_row + 1];
+        cur[0] = 0.0 + poured;
         for(int i = 1; i <= query_row; i++) {
-            List<Double> next = new ArrayList<>();
-            next.add(0.0);
-            for(int j = 0; j < cur.size(); j++) {
-                Double curPoured = cur.get(j) < 1.0 ? 0 : (cur.get(j) - 1) / 2;
-                
-                next.set(j, next.get(j) + curPoured);
-                next.add(curPoured);
+            Double[] next = new Double[query_row + 1];
+            next[0] = 0.0;
+            for(int j = 0; j < i; j++) {
+                Double curPoured = cur[j] < 1.0 ? 0.0 : (cur[j] - 1) / 2;
+                next[j] = next[j] + curPoured;
+                next[j+1] = curPoured;
             }
 
             cur = next;
         }
 
-        return Math.min(cur.get(query_glass), 1.0);
+        return Math.min(cur[query_glass], 1.0);
     }
 }
