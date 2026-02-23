5bcb207deff95059c0a39a82b5cf39fae769b01c
irv-05 <irv05code@gmail.com>
2026-02-23 15:39:07 -0800

    Time: 5 ms (76.90%) | Memory: 46.1 MB (32.95%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -4,27 +4,13 @@ class Solution {
             return false;
         }
 
-        int a = 10;
-        int b = 1;
-        while(x/b >= 10) {
-            b*=10;
+        int xflip = 0;
+        int i = x;
+        while(i != 0) {
+            xflip = (xflip*10) + i%10;
+            i/=10;
         }
 
-        while(b > 1) {
-            int left = x/b;
-            int right = x%a;
-            if(left != right) {
-                return false;
-            }
-
-            if(left!=0) {
-                x%=(left*b);
-            }
-
-            x/=10;
-            b/=100;
-        }
-
-        return true;
+        return xflip == x;
     }
 }
