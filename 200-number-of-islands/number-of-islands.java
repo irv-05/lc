1f2c045c2a498f25d3bd619429205c0d2cbb483b
irv-05 <irv05code@gmail.com>
2026-02-26 17:15:06 -0800

    Time: 5 ms (31.48%) | Memory: 51.4 MB (91.90%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for(int i = 0; i < grid[0].length; i++) {
+            for(int j = 0; j < grid.length; j++) {
+                if(grid[j][i] == '1') {
+                    count++;
+                    traverse(grid, j, i);
+                }
+            }
+        }
+        return count;
+    }
+
+    public void traverse(char[][] grid, int x, int y) {
+        Pair<Integer, Integer> coord = new Pair<>(x, y);
+        Deque<Pair<Integer,Integer>> stack = new ArrayDeque<>();
+        stack.push(coord);
+        while(!stack.isEmpty()) {
+            Pair<Integer,Integer> cur = stack.pop();
+            x = cur.getKey();
+            y = cur.getValue();
+            grid[x][y] = 'v';
+
+            if(x - 1 >= 0 && grid[x-1][y] == '1') {
+                stack.push(new Pair<>(x-1, y));
+            }
+
+            if(x + 1 < grid.length && grid[x+1][y] == '1') {
+                stack.push(new Pair<>(x+1, y));
+            }
+
+            if(y - 1 >= 0 && grid[x][y-1] == '1') {
+                stack.push(new Pair<>(x, y - 1));
+            }
+
+            if(y + 1 < grid[0].length && grid[x][y+1] == '1') {
+                stack.push(new Pair<>(x, y + 1));
+            }
+        }
+    }
+}
