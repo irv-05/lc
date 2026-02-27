61fdff5839065589bed2a8a607273407955e89e1
irv-05 <irv05code@gmail.com>
2026-02-26 17:20:24 -0800

    Time: 3 ms (87.42%) | Memory: 52.5 MB (33.00%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -5,7 +5,7 @@ class Solution {
             for(int j = 0; j < grid.length; j++) {
                 if(grid[j][i] == '1') {
                     count++;
-                    traverse(grid, j, i);
+                    traverseRec(grid, j, i);
                 }
             }
         }
@@ -38,5 +38,24 @@ class Solution {
                 stack.push(new Pair<>(x, y + 1));
             }
         }
+    }
+
+    public void traverseRec(char[][] grid, int x , int y) {
+            grid[x][y] = 'v';
+            if(x - 1 >= 0 && grid[x-1][y] == '1') {
+                traverseRec(grid, x-1, y);
+            }
+
+            if(x + 1 < grid.length && grid[x+1][y] == '1') {
+                traverseRec(grid, x+1, y);
+            }
+
+            if(y - 1 >= 0 && grid[x][y-1] == '1') {
+                traverseRec(grid, x, y - 1);
+            }
+
+            if(y + 1 < grid[0].length && grid[x][y+1] == '1') {
+                traverseRec(grid, x, y + 1);
+            }
     }
 }
