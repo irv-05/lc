aca9bd51a21802d33ca65b874a3f4a8dd202651a
irv-05 <irv05code@gmail.com>
2026-02-27 14:28:20 -0800

    Time: 4 ms (57.65%) | Memory: 46.2 MB (97.31%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static Map<Character, Integer> map = new HashMap<>();
+    {
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+    }
+    
+    public int romanToInt(String s) {
+        int num = 0;
+        for(int i = 0; i < s.length(); i++) {
+            int curVal = map.get(s.charAt(i));
+            if(i+1 < s.length() && curVal < map.get(s.charAt(i+1))) {
+                num -= curVal;
+            } else {
+                num += curVal;
+            }
+        }
+        return num;
+    }
+}
