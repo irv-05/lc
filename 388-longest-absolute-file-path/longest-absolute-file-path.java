75446d9231b2eceeb16e6193a5c2ef63934b3be7
irv-05 <irv05code@gmail.com>
2026-02-27 17:30:42 -0800

    Time: 2 ms (20.58%) | Memory: 42.8 MB (61.99%) - LeetSync

--- 388-longest-absolute-file-path/longest-absolute-file-path.java
+++ 388-longest-absolute-file-path/longest-absolute-file-path.java
@@ -1,9 +1,12 @@
 class Solution {
     public int lengthLongestPath(String input) {
-        String maxPath = "";
+        Deque<String> stack = new ArrayDeque<>();
+
+        int maxPath = 0;
         int level = -1;
         input = input + '\n';
         StringBuilder curName = new StringBuilder();
+        int curLength = 0;
         StringBuilder curPath = new StringBuilder();
         int curLevel = 0;
         for(int i = 0; i < input.length(); i++) {
@@ -14,24 +17,20 @@ class Solution {
                     int toPop = level - curLevel;
 
                     for(int j = 0; j < toPop; j++) {
-                        if(curPath.length() !=0) {
-                            curPath.deleteCharAt(curPath.length() - 1);
-                        }
-
-                        while(curPath.length() != 0 && curPath.charAt(curPath.length() - 1) != '/') {
-                            curPath.deleteCharAt(curPath.length() - 1);
-                        }
+                        String dir = stack.pop();
+                        curLength -= dir.length();
                     }
                 } 
 
                 if(curName.toString().contains(".")) {
-                    if(curPath.length() + curName.length() > maxPath.length()) {
-                        maxPath = curPath.toString() + curName.toString();
+                    if(curLength + curName.length() > maxPath) {
+                        maxPath = curLength + curName.length();
                     }
                     level = curLevel;
                 } else {
-                    curPath.append(curName.toString());
-                    curPath.append("/");
+                    curName.append("/");
+                    stack.push(curName.toString());
+                    curLength += curName.length();
                     level = curLevel+1;     
                 }
                 
@@ -41,6 +40,6 @@ class Solution {
                 curName.append(input.charAt(i));
             }
         }
-        return maxPath.length();
+        return maxPath;
     }
 }
