18553d16e6a9b6e65f7ace6bc0651d72d7334260
irv-05 <irv05code@gmail.com>
2026-02-27 17:22:56 -0800

    Time: 5 ms (5.30%) | Memory: 42.8 MB (61.99%) - LeetSync

---
+++ 388-longest-absolute-file-path/longest-absolute-file-path.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int lengthLongestPath(String input) {
+        String maxPath = "";
+        int level = -1;
+        input = input + '\n';
+        StringBuilder curName = new StringBuilder();
+        StringBuilder curPath = new StringBuilder();
+        int curLevel = 0;
+        for(int i = 0; i < input.length(); i++) {
+            if('\t' == input.charAt(i)) {
+                curLevel++;
+            } else if('\n' == input.charAt(i)) {
+                if(curLevel < level) {
+                    int toPop = level - curLevel;
+
+                    for(int j = 0; j < toPop; j++) {
+                        if(curPath.length() !=0) {
+                            curPath.deleteCharAt(curPath.length() - 1);
+                        }
+
+                        while(curPath.length() != 0 && curPath.charAt(curPath.length() - 1) != '/') {
+                            curPath.deleteCharAt(curPath.length() - 1);
+                        }
+                    }
+
+                    System.out.println(curPath);
+                } 
+
+                if(curName.toString().contains(".")) {
+                    if(curPath.length() + curName.length() > maxPath.length()) {
+                        maxPath = curPath.toString() + curName.toString();
+                    }
+                    level = curLevel;
+                } else {
+                    curPath.append(curName.toString());
+                    curPath.append("/");
+                    level = curLevel+1;     
+                }
+                
+                curLevel = 0;
+                curName = new StringBuilder();
+            } else {
+                curName.append(input.charAt(i));
+            }
+        }
+
+        System.out.println(maxPath);
+        return maxPath.length();
+    }
+}
