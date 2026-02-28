134459322faa6e13352becd3d907c97646f907a0
irv-05 <irv05code@gmail.com>
2026-02-27 17:36:46 -0800

    Time: 2 ms (20.58%) | Memory: 42.6 MB (89.52%) - LeetSync

--- 388-longest-absolute-file-path/longest-absolute-file-path.java
+++ 388-longest-absolute-file-path/longest-absolute-file-path.java
@@ -3,9 +3,11 @@ class Solution {
         Deque<Integer> stack = new ArrayDeque<>();
         int maxPath = 0;
         int level = -1;
+
         StringBuilder curName = new StringBuilder();
         int curLength = 0;
         int curLevel = 0;
+        boolean isFile = false;
         for(int i = 0; i < input.length()+1; i++) {
             if(i == input.length() || '\n' == input.charAt(i)) {
                 if(curLevel < level) {
@@ -17,7 +19,7 @@ class Solution {
                     }
                 } 
 
-                if(curName.toString().contains(".")) {
+                if(isFile) {
                     if(curLength + curName.length() > maxPath) {
                         maxPath = curLength + curName.length();
                     }
@@ -30,9 +32,13 @@ class Solution {
                 
                 curLevel = 0;
                 curName = new StringBuilder();
+                isFile = false;
             } else if('\t' == input.charAt(i)) {
                 curLevel++;
             } else {
+                if(input.charAt(i) == '.') {
+                    isFile = true;
+                }
                 curName.append(input.charAt(i));
             }
         }
