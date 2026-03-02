8b6cfc06496d77cee0bc50263320cddb67fcc76d
irv-05 <irv05code@gmail.com>
2026-03-02 15:20:28 -0800

    Time: 1 ms (87.97%) | Memory: 44.2 MB (76.05%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -4,17 +4,16 @@ class Solution {
         //empty list represents the first subset that is present in all sets and also represents the base case
         List<Integer> base = new ArrayList<>();
         subsets.add(base);
-        
+
         //we start with set at index [0,1], then move on to set at index [0,2]...[0,n]. At each point, ever list we have found needs to duplicate itself and add the current index,
         //to satisfy the property: "for every new index, we either add the number or we don't".
         for(int i = 0; i < nums.length; i++) {
-            List<List<Integer>> curSubsets = new ArrayList<>();
-            for(List<Integer> cur : subsets) {
-                List<Integer> dup = new ArrayList<>(cur);
+            int curLen = subsets.size();
+            for(int j = 0; j < curLen; j++) {
+                List<Integer> dup = new ArrayList<>(subsets.get(j));
                 dup.add(nums[i]);
-                curSubsets.add(dup);
+                subsets.add(dup);
             }
-            subsets.addAll(curSubsets);
         }
 
         return subsets;
