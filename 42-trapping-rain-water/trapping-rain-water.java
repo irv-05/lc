e124fff91252778187e6a93bc45ba362037e1543
irv-05 <irv05code@gmail.com>
2026-03-02 18:08:54 -0800

    Time: 1 ms (60.31%) | Memory: 47.5 MB (92.01%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -32,33 +32,24 @@ class Solution {
             }
         }
 
-        System.out.println("level is" + level);
-System.out.println(water);
         while(a < b) {
             if(height[a] <= level) {
                 a++;
                 if(a!=b) {
                     water -= Math.min(height[a], level);
                 }
-                System.out.println("a" + water);
             } else if(height[b] <= level) {
                 b--;
                 if(a!=b) {
                     water -= Math.min(height[b], level);
                 }
-                System.out.println("b" + water);
 
             } else {
                 int oldLevel = level;
                 level = Math.min(height[a], height[b]);
                 water += (b - a - 1) * (level-oldLevel);
-                System.out.println(water);
             }
         }
-
-        /*if(a == b && a > 0 && a < height.length-1) {
-            water -= Math.min(height[a], level);
-        }*/
 
         return water;
     }
