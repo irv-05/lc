ec5b5976de5736bd051ee300596843fb028e7618
irv-05 <irv05code@gmail.com>
2026-03-31 17:59:08 -0700

    Time: 0 ms (100.00%) | Memory: 44.6 MB (88.18%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -2,7 +2,7 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int a = 0;
         int b = nums.length-1;
-        while(a < b) {
+        while(a <= b) {
             //prevent overflow
             int mid = a + ((b - a) / 2);
 
@@ -11,10 +11,10 @@ class Solution {
             } else if(nums[mid] < target) {
                 a = mid + 1;
             } else {
-                b = mid;
+                b = mid - 1;
             }
         }
 
-        return nums[a] >= target ? a : a + 1;
+        return a;
     }
 }
